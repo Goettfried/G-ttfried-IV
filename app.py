deed6597bad347962633de0993ba32b6e32fc38b
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-01 19:51:04 +0200

    Add Flask-Migrate and update app configuration for migrations

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///form_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class FormData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    type = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    message = db.Column(db.Text)
+    type = db.Column(db.String(50))
 
 @app.route('/')
 def index():
     travail_data = FormData.query.filter_by(type="Je recherche du travail").all()
-    stage_data = FormData.query.filter_by(type="Je recherche un stage").all()
-    return render_template('index.html', travail_data=travail_data, stage_data=stage_data)
+    service_data = FormData.query.filter_by(type="Je propose mes services").all()
+    return render_template('index.html', travail_data=travail_data, service_data=service_data)
 
 @app.route('/init_db')
 def init_db():
@@ -29,14 +30,8 @@ def init_db():
 
 @app.route('/check_tables')
 def check_tables():
-    try:
-        conn = sqlite3.connect('form_data.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cursor.fetchall()
-        return f"Tables: {tables}"
-    except Exception as e:
-        return str(e)
+    tables = db.engine.table_names()
+    return f"Tables: {tables}"
 
 if __name__ == '__main__':
     app.run(debug=True)
