fa8dd8445c232be17a68119d0406535dbdf3b767
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-01 03:02:27 +0200

    Initial commit with Flask app and configurations

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import os
 from flask import Flask, request, jsonify
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import os
 
 app = Flask(__name__)
 
@@ -17,34 +17,59 @@ def receive_form():
     email = data.get('email')
     phone = data.get('phone')
     message = data.get('message')
+    form_type = data.get('type')
 
-    # Logique pour traiter les données et envoyer un email
-    print(f"Received data: {name}, {email}, {phone}, {message}")
+    if not all([name, email, message, form_type]):
+        return jsonify({'status': 'error', 'message': 'Missing required fields'}), 400
 
-    # Envoyer un email avec les données reçues
-    send_email(name, email, phone, message)
+    send_email(name, email, phone, message, form_type)
 
     return jsonify({'status': 'success', 'message': 'Form data received'}), 200
 
-def send_email(name, email, phone, message):
+def send_email(name, email, phone, message, form_type):
     sender_email = os.getenv('EMAIL_USER')
-    receiver_email = "destinataire@example.com"
+    receiver_email = os.getenv('EMAIL_USER')
     password = os.getenv('EMAIL_PASS')
+
+    subject = ""
+    text = ""
+    if form_type == "Je recherche du personnel":
+        subject = "Merci pour votre démarche - Recherche de personnel"
+        text = f"""
+        Bonjour {name} ! 
+
+        Je vous souhaite la bienvenue, merci pour votre démarche. 
+        Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes conditions générales : https://welshrecrutement.netlify.app/conditions_generales_nicolas_ballu.pdf. 
+        Si vous êtes plutôt intéressé(e) par de la location de services, ignorez ça pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. 
+
+        Je me réjouis de faire affaire avec vous.
+
+        Meilleures salutations.
+        """
+    else:
+        subject = "Merci pour votre démarche - Recherche de travail"
+        text = f"""
+        Bonjour {name} ! 
+
+        Je vous souhaite la bienvenue, merci pour votre démarche. 
+        Je vais prendre contact avec vous très prochainement. 
+        Pour aller de l'avant, j'aimerais vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. 
+        Je serais enchanté de vous aider.
+
+        Meilleures salutations.
+        """
 
     msg = MIMEMultipart()
     msg['From'] = sender_email
-    msg['To'] = receiver_email
-    msg['Subject'] = "Nouveau message de contact"
-
-    body = f"Nom: {name}\nEmail: {email}\nTéléphone: {phone}\nMessage: {message}"
-    msg.attach(MIMEText(body, 'plain'))
+    msg['To'] = email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(text, 'plain'))
 
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(sender_email, password)
-        text = msg.as_string()
-        server.sendmail(sender_email, receiver_email, text)
+        server.sendmail(sender_email, email, msg.as_string())
         server.quit()
         print("Email envoyé avec succès")
     except Exception as e:
