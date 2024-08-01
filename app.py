3ac72a85b1f5feb79e046c04427033e3232efc4d
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-01 16:00:08 +0200

    Updated app.py and added index.html

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ import os
 app = Flask(__name__)
 CORS(app)
 
+# Stocker les données reçues
+form_data = {
+    "Je recherche du travail": [],
+    "Je recherche du personnel": []
+}
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=form_data)
 
 @app.route('/receive_form', methods=['POST'])
 def receive_form():
     data = request.get_json()
     if data:
+        form_type = data.get('type')
+        if form_type in form_data:
+            form_data[form_type].append(data)
         print(f"Received data: {data}")
         return jsonify({"status": "success", "message": "Form data received"}), 200
     return jsonify({"status": "error", "message": "No data received"}), 400
