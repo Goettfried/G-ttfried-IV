f4624694a5d588c4222f802a15a4bcbe7273bf04
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-02 03:01:46 +0200

    Mise à jour de l'authentification pour éviter la double identification

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
-from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.secret_key = os.getenv("SECRET_KEY")
 
 auth = HTTPBasicAuth()
 
 users = {
-    os.environ['USERNAME']: generate_password_hash(os.environ['PASSWORD'])
+    os.getenv("USERNAME"): os.getenv("PASSWORD")
 }
 
 @auth.verify_password
@@ -19,7 +19,9 @@ def verify_password(username, password):
     return None
 
 @app.route('/')
-def index():
+def home():
+    if 'username' in session:
+        return redirect(url_for('protected'))
     return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -27,25 +29,22 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if verify_password(username, password):
-            session['logged_in'] = True
+        if auth.verify_password(username, password):
+            session['username'] = username
             return redirect(url_for('protected'))
         else:
-            return 'Invalid credentials', 401
+            return 'Invalid credentials'
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/protected')
 @auth.login_required
 def protected():
-    return '''
-        Logged in successfully<br>
-        <a href="/logout">Logout</a>
-    '''
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    return redirect(url_for('login'))
+    return 'Logged in successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
