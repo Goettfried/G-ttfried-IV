4114fea287a324efcfa89dc9dac566667f079db0
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-02 02:09:23 +0200

    Correction de l'authentification pour vérifier le mot de passe

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_httpauth import HTTPBasicAuth
-from auth import auth
 import os
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+auth = HTTPBasicAuth()
+
+users = {
+    os.environ['USERNAME']: os.environ['PASSWORD']
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    return None
 
 @app.route('/')
 def index():
@@ -15,7 +27,7 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if auth.authenticate(username, password):
+        if verify_password(username, password):
             return redirect(url_for('protected'))
         else:
             return 'Invalid credentials', 401
