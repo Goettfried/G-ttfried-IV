19b6c43e363d01338ae6c8031bf37b81513c2475
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-03 00:49:24 +0200

    Changement de DATABASE_URI en DATABASE_URL et ajout de logs pour le débogage

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your_secret_key')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///instance/app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///instance/app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -37,14 +37,19 @@ class FormData(db.Model):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.check_password(password):
-            session['username'] = user.username
-            return redirect(url_for('index'))
-        else:
-            return 'Invalid username or password'
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            app.logger.debug(f"Trying to log in with username: {username}")
+            user = User.query.filter_by(username=username).first()
+            if user and user.check_password(password):
+                session['username'] = user.username
+                return redirect(url_for('index'))
+            else:
+                return 'Invalid username or password'
+        except Exception as e:
+            app.logger.error(f"Error during login: {e}")
+            return 'Internal Server Error', 500
     return render_template('login.html')
 
 @app.route('/logout')
@@ -68,7 +73,7 @@ def protected():
 def receive_form():
     try:
         data = request.get_json()
-        app.logger.error(f"Received data: {data}")  # Ajout de log pour capturer les données reçues
+        app.logger.debug(f"Received data: {data}")
         if data:
             form_data = FormData(
                 name=data.get('name'),
@@ -82,7 +87,7 @@ def receive_form():
             return jsonify({'status': 'success'})
         return jsonify({'status': 'error', 'message': 'Invalid data'}), 400
     except Exception as e:
-        app.logger.error(f"Database error: {e}")  # Ajout de log pour capturer les erreurs de la base de données
+        app.logger.error(f"Database error: {e}")
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 @app.route('/view_data')
