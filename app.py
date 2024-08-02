995306a10d3efb6ac0918b1818ba9163a533be52
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-08-02 04:05:21 +0200

    Correction du lien de déconnexion et mise à jour des fichiers

--- app.py
+++ app.py
@@ -1,44 +1,46 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
+app.secret_key = 'bf55b6617cb416f62fb5c63c6b874cfd'
+
+auth = HTTPBasicAuth()
 
 users = {
-    os.getenv("USERNAME"): os.getenv("PASSWORD")
+    "Nicolas": generate_password_hash('Goetzinger')
 }
 
-@app.route('/')
-def home():
-    if 'username' in session:
-        return redirect(url_for('protected'))
-    return redirect(url_for('login'))
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    return None
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if username in users and check_password_hash(users.get(username), password):
+        if auth.verify_password(username, password):
             session['username'] = username
             return redirect(url_for('protected'))
-        else:
-            return 'Invalid credentials'
+        return 'Invalid credentials'
     return render_template('login.html')
+
+@app.route('/protected')
+@auth.login_required
+def protected():
+    if 'username' in session:
+        return '''
+        <h1>Logged in successfully</h1>
+        <a href="{{ url_for('logout') }}">Se déconnecter</a>
+        '''
+    return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('login'))
-
-@app.route('/protected')
-def protected():
-    if 'username' in session:
-        return '''
-            Logged in successfully
-            <br><a href="{{ url_for('logout') }}">Se déconnecter</a>
-        '''
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
